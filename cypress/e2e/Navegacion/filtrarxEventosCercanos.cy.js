fd4d3324cc228f64e3f4ca18305ef4486ad0879a
Fabian Ceferino Gonzalez <ceferinomonier@gmail.com>
2025-09-18 18:22:07 -0300

    FILTRAR POR CERCANIA

--- cypress/e2e/Navegacion/filtrarxEventosCercanos.cy.js
+++ cypress/e2e/Navegacion/filtrarxEventosCercanos.cy.js
@@ -10,9 +10,13 @@ let  urlapi="https://vps-3696213-x.dattaweb.com/"
            cy.viewport(1400, 900); // Fuerza modo escritorio
 
            cy.visit(urlapi);
+           cy.wait(1000);
+           
 
-      cy.contains('locationFilter').click();
-                  cy.wait(300);
+      //cy.contains('locationFilter').first().click();
+       cy.get('[data-slot="input"]').last().click()
+       //cy.get('[aria-label=" "]').contains('locationFilter').first().click();
+                  cy.wait(1000);
 
 
           
